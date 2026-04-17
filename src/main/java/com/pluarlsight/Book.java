23d39ca98cd05f4e2072f7154a5221e5ab5af702
JopsonDev <jopsonmason@outlook.com>
2026-04-17 14:27:04 -0400

    Initial commit

---
+++ src/main/java/com/pluarlsight/Book.java
@@ -0,0 +1,14 @@
+package com.pluarlsight;
+
+public class Book {
+    //id:int
+    //isbn:String
+    //title:String
+    //isCheckedOut:boolean
+    //checkedOutTo:String
+
+    //method checkOut(name) = the books checkedOutTo variable should be set to the name provided
+    // and the isCheckedOut varible should be set to true
+
+    //method checkIn = checkedOutTo should be set too "" && isCheckedOut should be false
+}
---
+++ src/main/java/com/pluarlsight/Main.java
@@ -0,0 +1,14 @@
+package com.pluarlsight;
+
+public class Main {
+    static void main(String[] args) {
+        //hold a inventory of at least 20 books of my choice
+
+        //while loop for Store Home Screen
+        //store home screen
+        //1. show avaible books//prompt user to select book to check out or go back//if check out get name
+        //2. show checkout out books prompts user to check in book with c or go back with x
+            //checking in prompt user for id and go back to home screen
+
+    }
+}
